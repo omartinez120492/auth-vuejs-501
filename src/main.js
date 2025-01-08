2f1389355afe0a9ab1ea8b172ccfc9ced5a77b00
omartinez120492 <omar1204@gmail.com>
2025-01-08 11:53:14 -0600

    2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
+import {  createPinia } from 'pinia'
+
 import App from './App.vue'
 import { router } from './routes'
+import './style.css'
+
+const pinia = createPinia()
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app');
