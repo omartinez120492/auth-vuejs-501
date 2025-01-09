f5e8281e1e50de4a18c7fe352b53fe87040fe199
omartinez120492 <omar1204@gmail.com>
2025-01-09 11:30:40 -0600

    5

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,15 @@
-//TODO: Definir la tienda
+
+import { defineStore } from 'pinia'
+
+
+export default useUserStore = defineStore('userState', {
+
+    state: () => ({ user: null, uid: null }),
+
+    getters: {
+        isActiveUser: (state) => ( (state.user && state.uid) ? true : false )
+    }
+
+    
+
+});
